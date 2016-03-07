38a8b2ce81872aa00d1a54ce2b46fef6c9e1143d
Anthoni Gleeson <anthoni@glaroam2-144-222.wireless.gla.ac.uk>
2016-03-07 15:20:18 +0000

    updates

--- zombie_game_project/zombie/views.py
+++ zombie_game_project/zombie/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from zombie.models import UserProfile
+from engine.game import Game
 
 def index(request):
     context_dict = {}
     return render(request, 'zombie/index.html', context_dict)
 
 def play(request):
-    return HttpResponse("play")
+    g = Game()
+    g.start_new_day()
+    print g.game_state
+    print g.player_state
+    #if games exists
+    # load game is it over?
+    context_dict ={'player':g.player_state, 'game':g.game_state, 'turn_options': g.turn_options()}
+    return render(request,'zombie/play.html',context_dict)
 
 def leaderboards(request):
     mostDays = UserProfile.objects.order_by('-most_days_survived')[:20]
