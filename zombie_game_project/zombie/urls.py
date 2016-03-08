1621667f765851329ef6483e84f1fb23eb0fbef3
Anthoni <anthonigleeson96@gmail.com>
2016-03-08 19:21:58 +0000

    added basic game needs template

--- zombie_game_project/zombie/urls.py
+++ zombie_game_project/zombie/urls.py
@@ -4,7 +4,8 @@ from zombie import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^how_to_play/$', views.how_to_play, name='how_to_play'),
-        url(r'^play/', views.play, name='play'),
         url(r'^leaderboards/', views.leaderboards, name='leaderboards'),
         url(r'^user/$', views.user, name='user'),
+        url(r'^play/', views.play, name='play'),
+        url(r'^turn/?P<action>[A-Z]+/(?P<num>[0-9]+)/$', views.turn, name='turn')
                        )
--- zombie_game_project/zombie/views.py
+++ zombie_game_project/zombie/views.py
@@ -12,10 +12,36 @@ def play(request):
     g.start_new_day()
     print g.game_state
     print g.player_state
+    print g.street.get_current_house()
     #if games exists
     # load game is it over?
-    context_dict ={'player':g.player_state, 'game':g.game_state, 'turn_options': g.turn_options()}
+    context_dict ={'player':g.player_state, 'game':g.game_state, 'turn_options': g.turn_options(),
+                   'time_left':g.time_left, 'street': g.street , 'house_list': g.street.house_list,
+                   'current_house': g.street.get_current_house(),
+                   }
     return render(request,'zombie/play.html',context_dict)
+
+def turn(action,num):
+    g = Game()
+    if action == ['MOVE','SEARCH']:
+        g.take_turn(action, num)
+    else:
+        g.take_turn(action)
+    if g.game_state == 'STREET':
+        context_dict ={'player':g.player_state, 'game':g.game_state, 'turn_options': g.turn_options(),
+                   'time_left':g.time_left, 'street': g.street , 'house_list': g.street.house_list,
+                   'current_house': g.street.get_current_house(),
+                   }
+    elif g.game_state == 'HOUSE':
+        context_dict = {'player':g.player_state, 'game':g.game_state, 'turn_options': g.turn_options(),
+                    'time_left':g.time_left, 'current_house':g.street.get_current_house(),
+                    'current_room':g.street.get_current_house().get_current_room()
+        }
+    elif g.game_state == 'ZOMBIE':
+        context_dict = {'player':g.player_state, 'game':g.game_state, 'turn_options': g.turn_options(),
+                    'time_left':g.time_left, 'num_zombies':g.street.get_current_house().get_current_room().zombies,
+                        }
+    return render('zombie/play.html',context_dict)
 
 def leaderboards(request):
     mostDays = UserProfile.objects.order_by('-most_days_survived')[:20]
