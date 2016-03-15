9f829e5f8f0de37cdf4c24f61e1f79bf8b062558
Anthoni <anthonigleeson96@gmail.com>
2016-03-15 15:09:37 +0000

    everything

--- zombie_game_project/zombie/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from zombie.models import UserProfile ,Badge, Achievement
-
-admin.site.register(UserProfile)
-admin.site.register(Badge)
-admin.site.register(Achievement)
-
