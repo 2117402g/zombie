59ff523e75eb0d6d5e1432b17646a01ee4004280
Anthoni <anthonigleeson96@gmail.com>
2016-03-21 20:01:11 +0000

    test

--- zombie_game_project/zombie_game_project/urls.py
+++ zombie_game_project/zombie_game_project/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from registration.backends.simple.views import RegistrationView
+from django.conf.urls.static import static
 
 # Create a new class that redirects the user to the index page, if successful at logging
 
@@ -25,6 +26,8 @@ if settings.DEBUG:
         (r'^media/(?P<path>.*)',
         'serve',
         {'document_root': settings.MEDIA_ROOT}), )
+if not settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 
